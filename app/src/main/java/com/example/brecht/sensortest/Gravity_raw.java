029c5cb5936ca09e96beaace327bff98088a25dc
ErazerBrecht <brecht.carlier@student.ap.be>
2015-03-01 19:41:29 +0100

    Added Sampling Rate in Gravity

--- app/src/main/java/com/example/brecht/sensortest/Gravity_raw.java
+++ app/src/main/java/com/example/brecht/sensortest/Gravity_raw.java
@@ -21,9 +21,12 @@ public class Gravity_raw extends ActionBarActivity implements SensorEventListene
     private TextView x;
     private TextView y;
     private TextView z;
+    private TextView sampleRateText;
 
     private double startTime;
     private double elapsedTime;
+    private double oldElapsedTime;
+    private double sampleRate;
 
     private FileWriter f;
 
@@ -37,6 +40,8 @@ public class Gravity_raw extends ActionBarActivity implements SensorEventListene
         x = (TextView) findViewById(R.id.x);
         y = (TextView) findViewById(R.id.y);
         z = (TextView) findViewById(R.id.z);
+        sampleRateText = (TextView) findViewById(R.id.sampling);
+
 
         f = new FileWriter("Gravity_raw.txt", getApplicationContext());
         f.Write(getApplicationContext(), "Time;");
@@ -46,27 +51,22 @@ public class Gravity_raw extends ActionBarActivity implements SensorEventListene
         f.Write(getApplicationContext(), System.getProperty("line.separator"));
 
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        if (mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null){
+        if (mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null) {
             mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-            mSensorManager.registerListener(this, mSensor, mSensorManager.SENSOR_DELAY_NORMAL);
-        }
-        else {
+            mSensorManager.registerListener(this, mSensor, 999999999);
+        } else {
             //DO SHIT
         }
     }
 
     @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event)
-    {
-        if ((keyCode == KeyEvent.KEYCODE_BACK))
-        {
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK)) {
             //finish();
             android.os.Process.killProcess(android.os.Process.myPid());
         }
         return super.onKeyDown(keyCode, event);
     }
-
-
 
 
     @Override
@@ -75,13 +75,18 @@ public class Gravity_raw extends ActionBarActivity implements SensorEventListene
         y.setText(String.valueOf(event.values[1]));
         z.setText(String.valueOf(event.values[2]));
 
-        elapsedTime = (System.currentTimeMillis() /1000.0) - startTime ;
+        elapsedTime = (System.currentTimeMillis() / 1000.0) - startTime;
+        sampleRate = 1 / (elapsedTime - oldElapsedTime);
+
+        sampleRateText.setText(String.valueOf(sampleRate));
 
         f.Write(getApplicationContext(), String.valueOf(elapsedTime).replace(".", ",") + ";");
         f.Write(getApplicationContext(), String.valueOf(event.values[0]).replace(".", ",") + ";");
         f.Write(getApplicationContext(), String.valueOf(event.values[1]).replace(".", ",") + ";");
         f.Write(getApplicationContext(), String.valueOf(event.values[2]).replace(".", ",") + ";");
         f.Write(getApplicationContext(), System.getProperty("line.separator"));
+
+        oldElapsedTime = elapsedTime;
 
     }
 
