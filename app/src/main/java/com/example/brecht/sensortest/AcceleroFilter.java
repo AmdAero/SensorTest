9f738d6752bf0b55b37616ce8847dc001c6f5300
ErazerBrecht <brecht.carlier@student.ap.be>
2015-04-02 14:48:15 +0200

    Made it more readable Added private and final to the correct fields!

--- app/src/main/java/com/example/brecht/sensortest/AcceleroFilter.java
+++ app/src/main/java/com/example/brecht/sensortest/AcceleroFilter.java
@@ -4,57 +4,57 @@ package com.example.brecht.sensortest;
  * Created by Brecht on 1/04/2015.
  */
 public class AcceleroFilter {
-    double S1_Gain0 = 0.019370459361914474;
-    double S1_Gain2 = -0.83687937305767479;
-    double S1_Gain3 = 1.7593975356100171;
-    double S1_Gain4 = 2;
+    private final double S1_Gain0 = 0.019370459361914474;
+    private final double S1_Gain2 = -0.83687937305767479;
+    private final double S1_Gain3 = 1.7593975356100171;
+    private final double S1_Gain4 = 2;
 
-    double S1B1_Unit1 = 0;
-    double S1B1_Unit2 = 0;
-    double S1B2_Unit0 = 0;
-    double S1B2_Unit1 = 0;
-    double S1B2_Unit2 = 0;
-    double S1B3_Unit0 = 0;
-    double S1B3_Unit1 = 0;
-    double S1B3_Unit2 = 0;
-    double S1B1_Output;
-    double S1B2_Output;
-    double S1B3_Output;
-    double S1_Output;
+    private double S1B1_Unit1 = 0;
+    private double S1B1_Unit2 = 0;
+    private double S1B2_Unit0 = 0;
+    private double S1B2_Unit1 = 0;
+    private double S1B2_Unit2 = 0;
+    private double S1B3_Unit0 = 0;
+    private double S1B3_Unit1 = 0;
+    private double S1B3_Unit2 = 0;
+    private double S1B1_Output;
+    private double S1B2_Output;
+    private double S1B3_Output;
+    private double S1_Output;
 
-    double S2_Gain0 = 0.01711220663941166;
-    double S2_Gain2 = -0.62273174921388086;
-    double S2_Gain3 = 1.5542829226562345;
-    double S2_Gain4 = 2;
+    private final double S2_Gain0 = 0.01711220663941166;
+    private final double S2_Gain2 = -0.62273174921388086;
+    private final double S2_Gain3 = 1.5542829226562345;
+    private final double S2_Gain4 = 2;
 
-    double S2B1_Unit1 = 0 ;
-    double S2B1_Unit2 = 0 ;
-    double S2B2_Unit0 = 0;
-    double S2B2_Unit1 = 0 ;
-    double S2B2_Unit2 = 0 ;
-    double S2B3_Unit0 = 0;
-    double S2B3_Unit1 = 0;
-    double S2B3_Unit2 = 0;
-    double S2B1_Output;
-    double S2B2_Output;
-    double S2B3_Output;
-    double S2_Output;
+    private double S2B1_Unit1 = 0;
+    private double S2B1_Unit2 = 0;
+    private double S2B2_Unit0 = 0;
+    private double S2B2_Unit1 = 0;
+    private double S2B2_Unit2 = 0;
+    private double S2B3_Unit0 = 0;
+    private double S2B3_Unit1 = 0;
+    private double S2B3_Unit2 = 0;
+    private double S2B1_Output;
+    private double S2B2_Output;
+    private double S2B3_Output;
+    private double S2_Output;
 
-    double S3_Gain0 = 0.12799483651485619;
-    double S3_Gain3 = 0.74401032697028757;
+    private final double S3_Gain0 = 0.12799483651485619;
+    private final double S3_Gain3 = 0.74401032697028757;
 
-    double S3B1_Unit1 = 0 ;
-    double S3B1_Unit2 = 0 ;
-    double S3B2_Unit0 = 0;
-    double S3B2_Unit1 = 0 ;
-    double S3B2_Unit2 = 0 ;
-    double S3B3_Unit0 = 0;
-    double S3B3_Unit1 = 0;
-    double S3B3_Unit2 = 0;
-    double S3B1_Output;
-    double S3B2_Output;
-    double S3B3_Output;
-    double S3_Output;
+    private double S3B1_Unit1 = 0;
+    private double S3B1_Unit2 = 0;
+    private double S3B2_Unit0 = 0;
+    private double S3B2_Unit1 = 0;
+    private double S3B2_Unit2 = 0;
+    private double S3B3_Unit0 = 0;
+    private double S3B3_Unit1 = 0;
+    private double S3B3_Unit2 = 0;
+    private double S3B1_Output;
+    private double S3B2_Output;
+    private double S3B3_Output;
+    private double S3_Output;
 
     public double Filter(float input)
     {
@@ -63,20 +63,20 @@ public class AcceleroFilter {
         return SectionThree();
     }
 
-    public void SectionOne(float Input)
+    private void SectionOne(float Input)
     {
         //Section 1:
         //Block 1
-        S1B1_Output = (Input*S1_Gain0) + (S1B1_Unit1*S1_Gain2) + (S1B1_Unit2);
+        S1B1_Output = (Input * S1_Gain0) + (S1B1_Unit1 * S1_Gain2) + (S1B1_Unit2);
         S1B1_Unit1 = S1B1_Unit2;
         S1B1_Unit2 = S1B1_Output;
         //Block 2
-        S1B2_Output = (S1B2_Unit0*S1_Gain4) + (S1B2_Unit1*S1_Gain2) + (S1B2_Unit2*S1_Gain3);
+        S1B2_Output = (S1B2_Unit0 * S1_Gain4) + (S1B2_Unit1 * S1_Gain2) + (S1B2_Unit2 * S1_Gain3);
         S1B2_Unit1 = S1B2_Unit2;
         S1B2_Unit2 = S1B2_Output;
         S1B2_Unit0 = Input * S1_Gain0;
         //Block 3
-        S1B3_Output = (S1B3_Unit0) + (S1B3_Unit1*S1_Gain2) + (S1B3_Unit2*S1_Gain3);
+        S1B3_Output = (S1B3_Unit0) + (S1B3_Unit1 * S1_Gain2) + (S1B3_Unit2 * S1_Gain3);
         S1B3_Unit1 = S1B3_Unit2;
         S1B3_Unit2 = S1B3_Output;
         S1B3_Unit0 = S1B2_Unit0;
@@ -84,20 +84,20 @@ public class AcceleroFilter {
         S1_Output= S1B1_Output + S1B2_Output + S1B3_Output;
     }
 
-    public void SectionTwo()
+    private void SectionTwo()
     {
         //Section 2:
         //Block 1
-        S2B1_Output = (S1_Output*S2_Gain0) + S2B1_Unit1*S2_Gain2 + S2B1_Unit2*S2_Gain3;
+        S2B1_Output = (S1_Output * S2_Gain0) + (S2B1_Unit1 * S2_Gain2) + (S2B1_Unit2 * S2_Gain3);
         S2B1_Unit1 = S2B1_Unit2;
         S2B1_Unit2 = S2B1_Output;
         //Block 2
-        S2B2_Output = S2B2_Unit0*S2_Gain4 + S2B2_Unit1*S2_Gain2 + S2B2_Unit2*S2_Gain3;
+        S2B2_Output = (S2B2_Unit0 * S2_Gain4) + (S2B2_Unit1 * S2_Gain2) + (S2B2_Unit2 * S2_Gain3);
         S2B2_Unit1 = S2B2_Unit2;
         S2B2_Unit2 = S2B2_Output;
-        S2B2_Unit0 = S1_Output*S2_Gain0;
+        S2B2_Unit0 = S1_Output * S2_Gain0;
         //Block 3
-        S2B3_Output = S2B3_Unit0 + S2B3_Unit1*S2_Gain2 + S2B3_Unit2*S2_Gain3;
+        S2B3_Output = S2B3_Unit0 + (S2B3_Unit1 * S2_Gain2) + (S2B3_Unit2 * S2_Gain3);
         S2B3_Unit1 = S2B3_Unit2;
         S2B3_Unit2 = S2B3_Output;
         S2B3_Unit0 = S2B2_Unit0;
@@ -105,20 +105,20 @@ public class AcceleroFilter {
         S2_Output= S2B1_Output + S2B2_Output + S2B3_Output;
     }
 
-    public double SectionThree()
+    private double SectionThree()
     {
         //Section 3:
         //Block 1
-        S3B1_Output = S2_Output*S3_Gain0  + S3B1_Unit2*S3_Gain3;
+        S3B1_Output = (S2_Output * S3_Gain0)  + (S3B1_Unit2 * S3_Gain3);
         S3B1_Unit1 = S3B1_Unit2;
         S3B1_Unit2 = S3B1_Output;
         //Block 2
-        S3B2_Output = S3B2_Unit0 + S3B2_Unit2*S3_Gain3;
+        S3B2_Output = S3B2_Unit0 + (S3B2_Unit2 * S3_Gain3);
         S3B2_Unit1 = S3B2_Unit2;
         S3B2_Unit2 = S3B2_Output;
-        S3B2_Unit0 = S2_Output*S3_Gain0;
+        S3B2_Unit0 = S2_Output * S3_Gain0;
         //Block 3
-        S3B3_Output =  S3B3_Unit2*S3_Gain3;
+        S3B3_Output =  S3B3_Unit2 * S3_Gain3;
         S3B3_Unit1 = S3B3_Unit2;
         S3B3_Unit2 = S3B3_Output;
         S3B3_Unit0 = S3B2_Unit0;
@@ -128,6 +128,4 @@ public class AcceleroFilter {
 
         return S3_Output;
     }
-
-
 }
