8f3c5e4ce90b7a601fe428e553fdc6857b07bd65
ErazerBrecht <brecht.carlier@student.ap.be>
2015-04-07 16:27:54 +0200

    Testing accelero in combination with rotation

--- app/src/main/java/com/example/brecht/sensortest/AcceleroFragment.java
+++ app/src/main/java/com/example/brecht/sensortest/AcceleroFragment.java
@@ -21,13 +21,22 @@ import java.util.List;
 
 public class AcceleroFragment extends Fragment implements SensorEventListener,View.OnClickListener {
 
-
-    private SensorManager mSensorManager;
     private Sensor mSensor;
+    private SensorManager mSensorManager;
 
     private AcceleroFilter xFilter = new AcceleroFilter();
     private AcceleroFilter yFilter = new AcceleroFilter();
     private AcceleroFilter zFilter = new AcceleroFilter();
+
+    private double xFiltered;
+    private double yFiltered;
+    private double zFiltered;
+
+    private double azimuth;
+    private double pitch;
+    private double roll;
+    private float[] orientationValues = new float[3];
+    private float rotation[] = new float[16];
 
     private double startTime;
     private double elapsedTime;
@@ -70,15 +79,14 @@ public class AcceleroFragment extends Fragment implements SensorEventListener,Vi
         f = new FileWriter("Accelerometer.txt", getActivity().getApplicationContext());
 
         f.Write(v.getContext(), "Time;");
-        f.Write(v.getContext(), "X;");
-        f.Write(v.getContext(), "Y;");
-        f.Write(v.getContext(), "Z;");
+        f.Write(v.getContext(), "aX;");
+        f.Write(v.getContext(), "aY;");
         f.Write(v.getContext(), System.getProperty("line.separator"));
 
         mSensorManager = (SensorManager) getActivity().getSystemService(Context.SENSOR_SERVICE);
-        if (mSensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION) != null){
-            mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
-            mSensorManager.registerListener(this, mSensor,  mSensorManager.SENSOR_DELAY_GAME);
+        if (mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR) != null){
+            mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION),  SensorManager.SENSOR_DELAY_GAME);
+            mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR),  SensorManager.SENSOR_DELAY_GAME);
         }
         else {
             //DO SHIT
@@ -95,8 +103,10 @@ public class AcceleroFragment extends Fragment implements SensorEventListener,Vi
 
         sampleRateText.setText("?");
 
-        if (mSensorManager != null)
-            mSensorManager.unregisterListener(this, mSensor);
+        if (mSensorManager != null) {
+            mSensorManager.unregisterListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION));
+            mSensorManager.unregisterListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR));
+        }
 
         int i = 0;
         for(String s : list)
@@ -104,7 +114,7 @@ public class AcceleroFragment extends Fragment implements SensorEventListener,Vi
             i++;
             f.Write(getActivity().getApplicationContext(), s);
 
-            if (i % 4 == 0)
+            if (i % 3 == 0)
                 f.Write(getActivity().getApplicationContext(), System.getProperty("line.separator"));
         }
     }
@@ -130,23 +140,43 @@ public class AcceleroFragment extends Fragment implements SensorEventListener,Vi
 
     public void onSensorChanged(SensorEvent event)
     {
-        double xFiltered = xFilter.Filter(event.values[0]);
-        double yFiltered = yFilter.Filter(event.values[1]);
-        double zFiltered = zFilter.Filter(event.values[2]);
+        mSensor = event.sensor;
 
-        X.setText(String.valueOf(xFiltered));
-        Y.setText(String.valueOf(yFiltered));
-        Z.setText(String.valueOf(zFiltered));
 
-        elapsedTime = (System.currentTimeMillis() /1000.0) - startTime ;
-        sampleRate = 1 / (elapsedTime - oldElapsedTime);
-        sampleRateText.setText(String.valueOf(sampleRate));
 
-        list.add(String.valueOf(elapsedTime).replace(".", ",") + ";");
-        list.add(String.valueOf(xFiltered).replace(".", ",") + ";");
-        list.add(String.valueOf(yFiltered).replace(".", ",") + ";");
-        list.add(String.valueOf(zFiltered).replace(".", ",") + ";");
-        oldElapsedTime = elapsedTime;
+        if (mSensor.getType() == Sensor.TYPE_LINEAR_ACCELERATION) {
+            xFiltered = xFilter.Filter(event.values[0]);
+            yFiltered = yFilter.Filter(event.values[1]);
+            zFiltered = zFilter.Filter(event.values[2]);
+        }
+
+        else if(mSensor.getType() == Sensor.TYPE_ROTATION_VECTOR)
+        {
+            SensorManager.getRotationMatrixFromVector(rotation, event.values);
+            SensorManager.getOrientation(rotation, orientationValues);
+            double azimuth = Math.toDegrees(orientationValues[0]);
+            double pitch = Math.toDegrees(orientationValues[1]);
+            double roll = Math.toDegrees(orientationValues[2]);
+
+            double ax = xFiltered * Math.cos(roll) + zFiltered * Math.cos(90 - roll);
+            double ay = yFiltered * Math.cos(90 - pitch) + xFiltered * Math.cos(90 - roll) + zFiltered * Math.cos(pitch) * Math.cos(roll);
+
+            X.setText(String.valueOf(ax));
+            Y.setText(String.valueOf(ay));
+
+            elapsedTime = (System.currentTimeMillis() / 1000.0) - startTime;
+            sampleRate = 1 / (elapsedTime - oldElapsedTime);
+            sampleRateText.setText(String.valueOf(sampleRate));
+            oldElapsedTime = elapsedTime;
+
+            list.add(String.valueOf(elapsedTime).replace(".", ",") + ";");
+            list.add(String.valueOf(ax).replace(".", ",") + ";");
+            list.add(String.valueOf(ay).replace(".", ",") + ";");
+
+
+        }
+
+
 
     }
 
